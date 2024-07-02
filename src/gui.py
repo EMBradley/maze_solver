4a60a2b887350c5e7b132d98e84b9ed587fb2abe
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-07-01 21:06:00 -0400

    add line drawing

--- src/gui.py
+++ src/gui.py
@@ -1,29 +1,67 @@
+# pylint: disable=too-few-public-methods
 """Classes for creating and interacting with graphical interface"""
 
 from tkinter import Canvas, Tk
 
 
+class Point:
+    """
+    A point in a window that is `self.x` pixels from the left edge
+    and `self.y pixels from the top
+    """
+
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class Line:
+    """
+    A line segment to be drawn on screen with `self.points` as endpoints
+    """
+
+    def __init__(self, point_1: Point, point_2: Point) -> None:
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas: Canvas, fill_color: str = "black") -> None:
+        """Draw the line on the provided `Canvas`"""
+
+        canvas.create_line(
+            self.point_1.x,
+            self.point_1.y,
+            self.point_2.x,
+            self.point_2.y,
+            fill=fill_color,
+            width=2,
+        )
+
+
 class Window:
     """Graphical interface window"""
 
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         self.__root = Tk(className="Maze Solver")
         self.__canvas = Canvas(height=height, width=width)
         self.__canvas.pack()
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self):
+    def redraw(self) -> None:
         """Updates contents of window"""
         self.__root.update_idletasks()
         self.__root.update()
 
-    def wait_for_close(self):
-        """Continuously redraws window until it is closed"""
+    def wait_for_close(self) -> None:
+        """Keeps window open and continuously redraws it until it is closed"""
         self.__running = True
         while self.__running:
             self.redraw()
 
-    def close(self):
+    def close(self) -> None:
         """Closes window"""
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color: str = "black"):
+        """Draw a given line on the screen"""
+        line.draw(self.__canvas, fill_color)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 """Maze Solver: Generates and solves a maze"""
 
-from gui import Window
+from gui import Line, Point, Window
 
 
 def main():
     """Entry point for maze solver program"""
     window = Window(800, 800)
+    p = Point(2, 2)
+    q = Point(800, 800)
+    r = Point(2, 800)
+    s = Point(800, 2)
+
+    for a, b in [(p, q), (p, r), (p, s), (q, r), (q, s), (r, s)]:
+        window.draw_line(Line(a, b))
+
     window.wait_for_close()
 
 
