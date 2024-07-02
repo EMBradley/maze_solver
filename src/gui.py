5c3e3b90d70068992ccb3ac66a15652b39bdf80e
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-07-01 22:23:49 -0400

    add Maze class

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,7 @@
-# pylint: disable=too-few-public-methods
+# pylint: disable=[too-few-public-methods, too-many-arguments]
 """Classes for creating and interacting with graphical interface"""
 
+from time import sleep
 from tkinter import Canvas, Tk
 from typing import Self
 
@@ -38,67 +39,6 @@ class Line:
         )
 
 
-class Cell:
-    """
-    Defines a cell in the maze. The coordinates `x1` and `y1` are its top left corner.
-    The coordinates `x2` and `y2` are its bottom right corner
-    """
-
-    def __init__(
-        self,
-        x1: int,
-        y1: int,
-        x2: int,
-        y2: int,
-        # is_finish: bool = False,
-    ) -> None:
-        self.walls = {
-            "top": True,
-            "bottom": True,
-            "left": True,
-            "right": True,
-        }
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
-        # self.__is_finish = is_finish
-
-    def get_center(self):
-        """
-        Returns the point in the center of a cell,
-        rounding up and to the left
-        """
-        return Point((self.__x1 + self.__x2) // 2, (self.__y1 + self.__y2) // 2)
-
-    def draw(self, canvas: Canvas, fill_color: str = "black"):
-        """Draws the cell on the given canvas"""
-        if self.walls["top"]:
-            top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
-            top_wall.draw(canvas, fill_color)
-        if self.walls["bottom"]:
-            bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
-            bottom_wall.draw(canvas, fill_color)
-        if self.walls["left"]:
-            left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
-            left_wall.draw(canvas, fill_color)
-        if self.walls["right"]:
-            right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
-            right_wall.draw(canvas, fill_color)
-
-    def draw_move(self, to_cell: Self, canvas: Canvas, undo=False):
-        """
-        Draws a line from the center of `self` to the center of `to_cell`
-        """
-        if undo:
-            fill_color = "gray"
-        else:
-            fill_color = "red"
-        start = self.get_center()
-        end = to_cell.get_center()
-        Line(start, end).draw(canvas, fill_color)
-
-
 class Window:
     """Graphical interface window"""
 
@@ -128,10 +68,109 @@ class Window:
         """Draw a given line on the screen"""
         line.draw(self.__canvas, fill_color)
 
-    def draw_cell(self, cell: Cell, fill_color: str = "black"):
-        """Draw a given cell on the screen"""
-        cell.draw(self.__canvas, fill_color)
 
-    def draw_move(self, first_cell: Cell, second_cell: Cell, undo=False):
-        """Draw a line between the centers of the given cells"""
-        first_cell.draw_move(second_cell, self.__canvas, undo)
+class Cell:
+    """
+    Defines a cell in the maze. The coordinates `x1` and `y1` are its top left corner.
+    The coordinates `x2` and `y2` are its bottom right corner
+    """
+
+    def __init__(
+        self,
+        x1: int,
+        y1: int,
+        x2: int,
+        y2: int,
+        window: Window,
+    ) -> None:
+        self.walls = {
+            "top": True,
+            "bottom": True,
+            "left": True,
+            "right": True,
+        }
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        self.__window = window
+
+    def get_center(self):
+        """
+        Returns the point in the center of a cell,
+        rounding up and to the left
+        """
+        return Point((self.__x1 + self.__x2) // 2, (self.__y1 + self.__y2) // 2)
+
+    def draw(self):
+        """Draws the cell on the given canvas"""
+        if self.walls["top"]:
+            top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__window.draw_line(top_wall)
+        if self.walls["bottom"]:
+            bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__window.draw_line(bottom_wall)
+        if self.walls["left"]:
+            left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__window.draw_line(left_wall)
+        if self.walls["right"]:
+            right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__window.draw_line(right_wall)
+
+    def draw_move(self, to_cell: Self, undo=False):
+        """
+        Draws a line from the center of `self` to the center of `to_cell`
+        """
+        if undo:
+            fill_color = "gray"
+        else:
+            fill_color = "red"
+        start = self.get_center()
+        end = to_cell.get_center()
+        self.__window.draw_line(Line(start, end), fill_color)
+
+
+class Maze:
+    """Defines the entire maze"""
+
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        num_rows: int,
+        num_cols: int,
+        cell_size_x: int,
+        cell_size_y: int,
+        window: Window,
+    ) -> None:
+        self.top_left = (x, y)
+        self.dimensions = (num_rows, num_cols)
+        self.cell_size = (cell_size_x, cell_size_y)
+        self.window = window
+        self.__create_cells()
+
+    def __create_cells(self) -> None:
+        self.__cells = []
+
+        for j in range(self.dimensions[1]):
+            column = []
+            for i in range(self.dimensions[0]):
+                x1 = self.top_left[0] + self.cell_size[0] * i
+                y1 = self.top_left[1] + self.cell_size[1] * j
+                x2 = x1 + self.cell_size[0]
+                y2 = y1 + self.cell_size[1]
+                cell = Cell(x1, y1, x2, y2, self.window)
+                column.append(cell)
+            self.__cells.append(column)
+
+        for i in range(self.dimensions[0]):
+            for j in range(self.dimensions[1]):
+                self.__draw_cell(i, j)
+
+    def __draw_cell(self, i: int, j: int) -> None:
+        self.__cells[i][j].draw()
+        self.__animate()
+
+    def __animate(self):
+        self.window.redraw()
+        sleep(0.05)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,12 @@
 """Maze Solver: Generates and solves a maze"""
 
-from gui import Cell, Window
+from gui import Maze, Window
 
 
 def main():
     """Entry point for maze solver program"""
     window = Window(800, 800)
-    first = Cell(250, 250, 275, 275)
-    second = Cell(275, 250, 300, 275)
-
-    window.draw_cell(first)
-    window.draw_cell(second)
-    window.draw_move(first, second)
+    Maze(10, 10, 10, 10, 75, 75, window)
     window.wait_for_close()
 
 
