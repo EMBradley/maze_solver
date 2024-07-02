a5b5aafb96dbf1a04cc7028e39ce6a3d94c38fbd
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-07-02 17:05:11 -0400

    add test to check that mazes are solvable

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-docstring
 
 import unittest
+from random import randint
 
 from cell import Direction
 from maze import Maze
@@ -21,6 +22,14 @@ class Tests(unittest.TestCase):
         maze = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertFalse(maze.cells[0][0].walls[Direction.Up])
         self.assertFalse(maze.cells[num_rows - 1][num_cols - 1].walls[Direction.Down])
+
+    def test_solvable(self):
+        """Generate 100 mazes of random sizes and check that they have solutions"""
+        for _ in range(100):
+            num_rows = randint(10, 50)
+            num_cols = randint(10, 50)
+            maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+            self.assertTrue(maze.dfs())
 
 
 if __name__ == "__main__":
